aa01f5d1b040b58fb1f670adc30c9a3e30d0e59e
donpapitoskey <alarcon.juan@correounivalle.edu.co>
2021-03-25 21:17:39 -0500

    config files adjusted to new project name

--- android/app/src/main/java/com/firstapp/MainActivity.java
+++ android/app/src/main/java/com/firstapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.firstapp;
+package com.mealapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "firstApp";
+    return "mealApp";
   }
 }
--- ios/firstAppTests/firstAppTests.m
+++ ios/firstAppTests/firstAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface firstAppTests : XCTestCase
+@interface mealAppTests : XCTestCase
 
 @end
 
-@implementation firstAppTests
+@implementation mealAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
